74dcd064a7092cfa436b78cde68d92061983c3d7
fly <pingxxhxj@sina.cn>
2018-08-30 20:41:21 +0800

    date format

--- common/utils/flydate.js
+++ common/utils/flydate.js
@@ -32,3 +32,28 @@ export const getTimeDesc = (time = +new Date()) => {
   }
   return desc
 }
+
+/**
+ * 根据时间戳输出输出特定时间格式，new Date().getTime() > 0
+ * @param {Time} time 时间戳 new Date()
+ * @param {String} fmt 输出格式 'YYYY-MM-dd hh:mm:ss'或'MM-dd hh:mm'等
+ */
+export const dateFormat = (time, fmt) => {
+  if (/(y+)/.test(fmt)) {
+    fmt = fmt.replace(RegExp.$1, (time.getFullYear() + '').substr(4 - RegExp.$1.length))
+  }
+  let obj = {
+    'M+': time.getMonth() + 1,
+    'd+': time.getDate(),
+    'h+': time.getHours(),
+    'm+': time.getMinutes(),
+    's+': time.getSeconds()
+  }
+  for (let key in obj) {
+    if (new RegExp(`(${key})`).test(fmt)) {
+      let str = obj[key] + ''
+      fmt = fmt.replace(RegExp.$1, (RegExp.$1.length === 1) ? str : ('00' + str).substr(str.length))
+    }
+  }
+  return fmt
+}
